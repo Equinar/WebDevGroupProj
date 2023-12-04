2c875471bd63600b0f8b4f2d133efb264b6c4fa4
Ivy Leung <hleung47@my.centennialcollege.ca>
2023-12-03 22:31:50 -0500

    Cart list design and react plus minus function

--- frontEnd/src/components/Cart.jsx
+++ frontEnd/src/components/Cart.jsx
@@ -1,43 +1,66 @@
-import React from 'react';
-import '../styles/cart.css'
-import Image from '../assets/logo.jpg'
+import React, { useState } from 'react';
+import '../styles/cart.css';
+import Image from '../assets/logo.jpg';
 
 const Cart = () => {
-  return (
+  const initialItems = [
+    { id: 1, image: Image, shopName: 'Shop A', itemName: 'Item 1', count: 1, price: 50, totalPrice: 50 },
+    { id: 2, image: Image, shopName: 'Shop B', itemName: 'Item 2', count: 1, price: 50, totalPrice: 50 },
+    { id: 3, image: Image, shopName: 'Shop B', itemName: 'Item 3', count: 1, price: 50, totalPrice: 50 },
+    { id: 4, image: Image, shopName: 'Shop C', itemName: 'Item 4', count: 1, price: 50, totalPrice: 50 },
+  ];
 
-    <div class="main">
-      <div><p class="title">Shopping Cart</p>
+  const [items, setItems] = useState(initialItems);
+
+  const plusCount = (itemId) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, count: item.count + 1, totalPrice: (item.count + 1) * item.price } : item
+      )
+    );
+  };
+
+  const minusCount = (itemId) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId && item.count > 1
+          ? { ...item, count: item.count - 1, totalPrice: (item.count - 1) * item.price }
+          : item
+      )
+    );
+  };
+
+  return (
+    <div className="main">
+      <div>
+        <p className="title">Shopping Cart</p>
       </div>
-      <div class="Cart">
+      <div className="Cart">
         <ul>
-          <li><img className="producticon" src={Image} alt="product icon" />
-            <br></br>
-            <label class="shopinfo">Shop name</label>
-            <br></br>
-            <br></br>
-            <label class="itemname">Item name</label>
-          </li>
-          <li><img className="producticon" src={Image} alt="product icon" />
-            <br></br>
-            <label class="shopinfo">Shop name</label>
-            <br></br>
-            <br></br>
-            <label class="itemname">Item name</label>
-          </li>
-          <li><img className="producticon" src={Image} alt="product icon" />
-            <br></br>
-            <label class="shopinfo">Shop name</label>
-            <br></br>
-            <br></br>
-            <label class="itemname">Item name</label>
-          </li>
-          <li><img className="producticon" src={Image} alt="product icon" />
-            <br></br>
-            <label class="shopinfo">Shop name</label>
-            <br></br>
-            <br></br>
-            <label class="itemname">Item name</label>
-          </li>
+          {items.map((item) => (
+            <li key={item.id}>
+              <div>
+
+                <div>
+                  <img className="producticon" src={item.image} alt="product icon" />
+                  <br />
+                  <label className="shopinfo">{item.shopName}</label>
+                  <br />
+                  <div class="totalprice">
+                      <label className="price">${item.totalPrice}</label>
+                    </div>
+                  <div class="qtybutton">
+                    <button onClick={() => minusCount(item.id)}>-</button>
+                    {item.count}
+                    <button onClick={() => plusCount(item.id)}>+</button>
+                  </div>
+ 
+                  <br />
+                  <label className="itemname">{item.itemName}</label>
+                </div>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
