81fbe541b9a8c864e589b3f7113c339e25fe6286
Ivy Leung <hleung47@my.centennialcollege.ca>
2023-12-03 21:49:39 -0500

    shopping cart UI design

--- frontEnd/src/components/Cart.jsx
+++ frontEnd/src/components/Cart.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import '../styles/cart.css'
+import Image from '../assets/logo.jpg'
 
 const Cart = () => {
   return (
-    <div>
-        <p>I am Cart</p>
+
+    <div class="main">
+      <div><p class="title">Shopping Cart</p>
       </div>
+      <div class="Cart">
+        <ul>
+          <li><img className="producticon" src={Image} alt="product icon" />
+            <br></br>
+            <label class="shopinfo">Shop name</label>
+            <br></br>
+            <br></br>
+            <label class="itemname">Item name</label>
+          </li>
+          <li><img className="producticon" src={Image} alt="product icon" />
+            <br></br>
+            <label class="shopinfo">Shop name</label>
+            <br></br>
+            <br></br>
+            <label class="itemname">Item name</label>
+          </li>
+          <li><img className="producticon" src={Image} alt="product icon" />
+            <br></br>
+            <label class="shopinfo">Shop name</label>
+            <br></br>
+            <br></br>
+            <label class="itemname">Item name</label>
+          </li>
+          <li><img className="producticon" src={Image} alt="product icon" />
+            <br></br>
+            <label class="shopinfo">Shop name</label>
+            <br></br>
+            <br></br>
+            <label class="itemname">Item name</label>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
