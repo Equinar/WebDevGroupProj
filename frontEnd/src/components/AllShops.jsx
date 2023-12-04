49a4d75aa0f6b55705e35d614c0359d96041b155
Ivy Leung <hleung47@my.centennialcollege.ca>
2023-12-03 21:41:30 -0500

    All shops page UI design

--- frontEnd/src/components/AllShops.jsx
+++ frontEnd/src/components/AllShops.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import '../styles/allshops.css'
+import Image from '../assets/logo.jpg'
 
 const AllShops = () => {
   return (
-    <div>
-        <p>I am Allshops</p>
+
+    <div class="main">
+      <div><p class="title">Shop List</p>
       </div>
+      <div class="shoplist">
+        <ul>
+          <li><img className="shopicon" src={Image} alt="shop icon" />
+            <br></br>
+            <label class="shopinfo">Shop 1</label>
+            <br></br>
+            <br></br>
+            <label class="ownername">owner</label>
+          </li>
+          <li><img className="shopicon" src={Image} alt="shop icon" />
+            <br></br>
+            <label class="shopinfo">Shop 2</label>
+            <br></br>
+            <br></br>
+            <label class="ownername">owner</label>
+          </li>
+          <li><img className="shopicon" src={Image} alt="shop icon" />
+            <br></br>
+            <label class="shopinfo">Shop 3</label>
+            <br></br>
+            <br></br>
+            <label class="ownername">owner</label>
+          </li>          <li><img className="shopicon" src={Image} alt="shop icon" />
+            <br></br>
+            <label class="shopinfo">Shop 4</label>
+            <br></br>
+            <br></br>
+            <label class="ownername">owner</label>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
