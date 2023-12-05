40d6a5cfeda0f572b22aab1a26b1b6ac53340f0f
Ivy Leung <hleung47@my.centennialcollege.ca>
2023-12-04 23:08:57 -0500

    button colour changed

--- frontEnd/src/components/Login.jsx
+++ frontEnd/src/components/Login.jsx
@@ -97,7 +97,7 @@ function Login({ onLogin }) {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="flex w-full justify-center rounded-md bg-slate-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-slate-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Sign in
                             </button>
